d431ca6db489be4c32a05df4fabba5f98596263e
Elaf Alsaleh <elafgmail@gmail.com>
2026-06-22 16:44:40 +0300

    Add new API endpoints

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Elaf from your Node.js server 🌱');
+  res.end('Hello World from Node.js server');
 });
 
 const PORT = 3000;
